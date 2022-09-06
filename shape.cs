6057ee17c8843cf4775c84c37705a83732310309
vino04003 <89254939+vino04003@users.noreply.github.com>
2022-09-06 11:26:18 +0200

    Add files via upload

---
+++ shape.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+    public class shape
+    {
+        protected int width;
+        protected int height;
+
+        public shape(int width, int height) 
+        {
+        this.width = width;
+        this.height = height;
+        }
+    }
